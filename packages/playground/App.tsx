956c434ee548f914579b2a4ba7d9c62270cd25f6
Max Wolf <maxcharleswolf@gmail.com>
2021-05-07 16:52:22 -0600

    updated Flipper on android; removed safe area context;

--- packages/playground/App.tsx
+++ packages/playground/App.tsx
@@ -1,9 +1,4 @@
 import React from 'react'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { StorybookUi } from './storybook'
 
-export const App = () => (
-  <SafeAreaProvider>
-    <StorybookUi />
-  </SafeAreaProvider>
-)
+export const App = () => <StorybookUi />
